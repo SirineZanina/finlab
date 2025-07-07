4eea3160b69de4642623b893c41b621398ce2edd
SirineZanina <sirine.zanina@isitc.u-sousse.tn>
2025-07-07 01:07:25 +0100

    implement intial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans, Inter } from "next/font/google";
+import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+	  subsets: ["latin"],
+	  variable: "--font-plus-jakarta-sans",
+	  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Finlens",
+  description: "A modern finance app for managing your personal finances",
+  icons: {
+	icon: "/icons/logo.svg",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${plusJakartaSans.variable} ${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
