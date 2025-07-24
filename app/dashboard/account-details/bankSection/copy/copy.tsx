c7e6e9086aadfc37191ba464fbfcb09e2e94757c
SirineZanina <sirine.zanina@isitc.u-sousse.tn>
2025-07-24 09:48:53 +0100

    feat: refcater svgs into components in bank page

--- app/dashboard/account-details/bankSection/copy/copy.tsx
+++ app/dashboard/account-details/bankSection/copy/copy.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { CheckCopyIcon } from '@/components/assets/icons/checkCopyIcon';
+import { CopyIcon } from '@/components/assets/icons/copyIcon';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
 
@@ -22,40 +24,13 @@ const Copy = ({ title }: { title: string }) => {
       onClick={copyToClipboard}
     >
       <p className="line-clamp-1 w-full max-w-full text-xs font-medium text-black-2">
-        {title} test
+        {title}
       </p>
 
       {!hasCopied ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 size-4"
-        >
-          <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
-          <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path>
-        </svg>
+        <CopyIcon />
       ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 size-4"
-        >
-          <polyline points="20 6 9 17 4 12"></polyline>
-        </svg>
+        <CheckCopyIcon />
       )}
     </Button>
   );
